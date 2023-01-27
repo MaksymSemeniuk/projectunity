00c62df493bc2e2d609a08b2e7d323c9db481d3f
MaksymSemeniuk <72151675+MaksymSemeniuk@users.noreply.github.com>
2023-01-27 04:39:58 +0200

    1

---
+++ art/Assets/Scripts/Localization/LocalizableString.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Main scriptable object used for localization purposes
+/// </summary>
+[CreateAssetMenu(fileName = "New string",menuName = "Loc. String")]
+public class LocalizableString : ScriptableObject
+{
+
+    public string english;
+    public string ukraine;
+
+    public string GetString(Language language)
+    {
+        switch (language)
+        {
+            case Language.Ukraine:
+                return ukraine;
+            case Language.English:
+                return english;
+        }
+        return null;
+    }
+
+    [System.Serializable]
+
+    public enum Language { English, Ukraine};
+}
